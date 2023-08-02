cc3c7bb3c646fa2199d889f5b3152a3df73f6293
Simon Prophet <simon.s.prophet@outlook.com>
2023-08-02 21:30:34 +0100

    add js function to create tags

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const tagsEl = document.querySelector('#tags');
+const textarea = document.querySelector('#textarea');
+
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+  createTag(e.target.value);
+});
+
+function createTag(input) {
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() != '')
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = '';
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+}
